d9f6a0f6286722cbe38ed2358c70b22b1a0e9f39
Better <cydbetter@163.com>
2017-11-07 17:26:08 +0800

    common

--- e3-common/src/main/java/cn/e3mall/utils/ItemCatResult.java
+++ e3-common/src/main/java/cn/e3mall/utils/ItemCatResult.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 public class ItemCatResult implements Serializable {
 
+	/**
+	 * text：表示节点的名字
+	 * state：表示节点的状态，如果is_parent是1，表示有子节点，默认是close。
+	 * 		如果是0，表示是没有子节点，默认是open
+	 */
 	private Long id;
 	
 	private String text;
