af77350f90fa82e3d13e15ba85eab88c5f261d9c
Better <cydbetter@163.com>
2017-11-09 23:16:48 +0800

    修改图片浏览器不兼容问题

--- e3-manager-web/src/main/java/cn/e3mall/manager/controller/PicUploadController.java
+++ e3-manager-web/src/main/java/cn/e3mall/manager/controller/PicUploadController.java
@@ -3,10 +3,10 @@ package cn.e3mall.manager.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import cn.e3mall.utils.FastDFSClient;
+import cn.e3mall.utils.JsonUtils;
 import cn.e3mall.utils.PictureResult;
 
 @Controller
@@ -20,10 +20,12 @@ public class PicUploadController {
 	 * 请求参数：uploadFile
 	 * 返回参数:pictureResult
 	 */
+	/**
+	 * 由于kingEditor存在浏览器版本问题，只有谷歌还用，其他不好用，所以需要手动封装json，返回String
+	 */
 	
 	@RequestMapping("/pic/upload")
-	@ResponseBody
-	public PictureResult uploadPicture(MultipartFile uploadFile) {
+	public String uploadPicture(MultipartFile uploadFile) {
 		
 		PictureResult result = null;
 		String url = "";
@@ -40,15 +42,17 @@ public class PicUploadController {
 		
 		//拼接全部URL
 		url = IMAGE_SERVER_URL + fileUrl;
-		System.out.println(url);
 		
 		//返回上传结果 0 成功 1失败 
 		result = new PictureResult(0, url,"图片上传成功");
-		return result;
+		
+		return JsonUtils.objectToJson(result);
+		
 		} catch (Exception e) {
 
 			result = new PictureResult(1, url,"图片上传失败");
-			return result;
+			
+			return JsonUtils.objectToJson(result);
 		}
 		
 	}
